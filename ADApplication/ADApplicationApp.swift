3e9a6d125ce9222835520a6938b679ad012907b7
bulian <bulian@middlebury.edu>
2022-10-18 09:33:35 -0400

    Initial Commit

---
+++ ADApplication/ADApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ADApplicationApp.swift
+//  ADApplication
+//
+//  Created by Bridget Ulian on 10/18/22.
+//
+
+import SwiftUI
+
+@main
+struct ADApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
