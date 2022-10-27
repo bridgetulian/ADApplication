b3fc64576ecb02e5df37fafb70bb88bd77188d69
AbbyWright <afwright@middlebury.edu>
2022-10-27 16:16:59 -0400

    feedback view basics

--- ADApplication/FeedbackView.swift
+++ ADApplication/FeedbackView.swift
@@ -9,8 +9,21 @@ import Foundation
 import SwiftUI
 
 struct FeedbackView: View {
+        
     var body: some View{
         Text("Feedback")
+            .font(.title)
+        Text("Part of maintaining universal usability is asking for an implementing feedback. Please let us know if there is anything that could make our application more usable!")
+            .padding([.leading, .bottom, .trailing])
+        Form {
+            TextField("Name & Contact (optional):", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+            TextEditor(text: .constant("Placeholder"))
+                .frame(height: 400.0)
+    
+            Button("Submit") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+        }
     }
 }
 
