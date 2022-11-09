39cc729ecc191a867506e51b9a9272038aa2167b
bulian <bulian@middlebury.edu>
2022-11-09 14:39:53 -0500

    taking out a block of commented code

--- ADApplication/ContentView.swift
+++ ADApplication/ContentView.swift
@@ -13,49 +13,6 @@ struct ContentView: View {
     
     var body: some View {
         HomeView()
-        /*
-        
-        let drag = DragGesture()
-                    .onEnded {
-                        if $0.translation.width < -100 {
-                            withAnimation {
-                                self.showMenu = false
-                            }
-                        }
-                    }
-                
-        //return NavigationView {
-            NavigationView {
-                    GeometryReader { geometry in
-                        
-                        VStack {
-                            if self.showMenu == false {
-                                Text("Welcome to ADApplication")
-                                    .frame(width:geometry.size.width)
-                            }
-                            
-                            ZStack(alignment: .leading) {
-                                if self.showMenu {
-                                    MenuView()
-                                        .frame(width: geometry.size.width/2)
-                                        .transition(.move(edge: .leading))
-                                }
-                            }
-                            .gesture(drag)
-                        }
-                    }
-                        .navigationBarTitle("ADApplication", displayMode: .inline)
-                        .navigationBarItems(leading: (
-                            Button(action: {
-                                withAnimation {
-                                    self.showMenu.toggle()
-                                }
-                            }) {
-                                Image(systemName: "line.horizontal.3")
-                                    .imageScale(.large)
-                            }
-                        ))
-            }*/
     }
 }
 
