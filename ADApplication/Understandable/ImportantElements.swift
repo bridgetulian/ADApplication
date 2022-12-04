4f355ca8124d71535e75056fe02e08463eaf45df
bulian <bulian@middlebury.edu>
2022-12-04 16:28:22 -0500

    important elements screen

---
+++ ADApplication/Understandable/ImportantElements.swift
@@ -0,0 +1,20 @@
+//
+//  ImportantElements.swift
+//  ADApplication
+//
+//  Created by Bridget Ulian on 12/4/22.
+//
+
+import SwiftUI
+
+struct ImportantElements: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ImportantElements_Previews: PreviewProvider {
+    static var previews: some View {
+        ImportantElements()
+    }
+}
