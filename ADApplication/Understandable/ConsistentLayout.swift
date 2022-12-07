5aef3c249bb6d31376751e7975e8086ee66f834a
AbbyWright <afwright@middlebury.edu>
2022-12-07 12:32:25 -0500

    consistent layout

--- ADApplication/Understandable/ConsistentLayout.swift
+++ ADApplication/Understandable/ConsistentLayout.swift
@@ -20,7 +20,10 @@ struct ConsistentLayout: View {
                     .font(.title)
                     .padding()
                 Text("By ensuring that compontents appear in a consistent order for each page helps users navigate the application better. In particular, users quickly know where to look for navigation elements and important information.")
-                    .padding(.horizontal)
+                    .padding([.leading, .bottom, .trailing])
+                Text("Change these sample app headers through the menu button to see examples of consistent and inconsistent page layout.")
+                    .font(.subheadline)
+                    .padding(.bottom)
                 GroupBox(label: Text("Consistent Layout of Header")) {
                     if(!changed1) {
                         HStack {
@@ -34,7 +37,7 @@ struct ConsistentLayout: View {
                         Spacer()
                         Text("Page Title")
                         Spacer()
-                        Image(systemName: "plus.square.on.square")
+                        Image(systemName: "doc.text")
                     }
                     }
                     if(changed1) {
@@ -49,10 +52,10 @@ struct ConsistentLayout: View {
                         Spacer()
                         Text("New Page Title")
                         Spacer()
-                        Image(systemName: "plus.square.on.square")
+                        Image(systemName: "doc.text")
                     }
                     }
-                }.padding(.horizontal)
+                }.padding([.leading, .bottom, .trailing])
                 GroupBox(label: Text("Inconsistent Layout of Header")) {
                     if(!changed2) {
                         HStack {
@@ -66,12 +69,12 @@ struct ConsistentLayout: View {
                         Spacer()
                         Text("Page Title")
                         Spacer()
-                        Image(systemName: "plus.square.on.square")
+                        Image(systemName: "doc.text")
                     }
                     }
                     if(changed2) {
                         HStack {
-                            Image(systemName: "plus.square.on.square")
+                            Image(systemName: "doc.text")
                             Spacer()
                         Menu("Menu") {
                             Text("Menu Item 3")
